a902d592d8924b0690c1d08200e003023b14bfa6
Svyatoslav <chel_c@mail.ru>
2020-08-30 00:29:09 +0300

    30:10

--- check.php
+++ check.php
@@ -1,1 +1,16 @@
 <?php
+$email = $_POST['email'];
+$message = $_POST['message'];
+$error = '';
+
+    if (trim($email) == '')
+        $error = 'Введите ваш email';
+    else if (trim($message) == '')
+        $error = 'Введите само сообщение';
+    else if (trim($email) > 10)
+        $error = 'Сообщение должно быть более 10 символов';
+
+    if ($error != '') {
+        echo $error;
+        exit;
+    }
