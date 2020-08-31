c600e5c5a7a4e8612d5aad0653654c9869198523
Svyatoslav <chel_c@mail.ru>
2020-08-31 22:45:09 +0300

    finish

---
+++ 404.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <title>PHP web site</title>
+</head>
+<body>
+
+<?php require "blocks/header.php" ?>
+
+<div class="container mt-5">
+    <h3 class="mb-5">Ошибка 404</h3>
+</div>
+
+<?php require "blocks/footer.php" ?>
+
+</body>
+</html>
